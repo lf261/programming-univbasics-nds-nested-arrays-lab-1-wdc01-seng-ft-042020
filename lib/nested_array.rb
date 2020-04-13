6d22323d28da2842bade3b6f846612222d095b28
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-13 18:07:03 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -44,13 +44,9 @@ def sorted_matrix = [
 [ "Asparagus",
   "Avocadoes"
   "Grapes",
-   
-  "Strawberries",
   "Potatoes",
-
-  
+  "Strawberries",
   ]
-  
   
   ]
 end
@@ -58,10 +54,16 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
+  
+  return [matrix][row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
+  
+  [matrix][row][column] = new_value
+  
+  return matrix
 end
